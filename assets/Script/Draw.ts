e3e4189d446ae75eac0adaae6eb97dcdd23a0bdd
panpchen <panpchen@gmail.com>
2020-12-18 01:31:04 +0800

    data-201218-01:30

--- assets/Script/Draw.ts
+++ assets/Script/Draw.ts
@@ -8,47 +8,47 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { DrawManager, PenType } from './DrawManager'
+import { DrawManager, PenType } from "./DrawManager";
 
-const { ccclass, property } = cc._decorator
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
   @property(cc.Label)
-  label: cc.Label = null
+  label: cc.Label = null;
 
   @property
-  text: string = 'hello'
+  text: string = "hello";
 
   @property(cc.Node)
-  board: cc.Node = null
+  board: cc.Node = null;
 
-  private drawManager: DrawManager
+  private drawManager: DrawManager;
 
   // LIFE-CYCLE CALLBACKS:
 
   // onLoad () {}
 
   start() {
-    this.drawManager = new DrawManager()
-    this.drawManager.init(this.board)
-    this.onPen()
-    this.node.addChild(this.drawManager.drawNode)
+    this.drawManager = new DrawManager();
+    this.drawManager.init(this.board);
+    this.onPen();
+    this.node.addChild(this.drawManager.drawNode);
   }
 
   onEraser(e, str) {
-    if (str === 'clear') {
-      this.drawManager.setStrokeColor(new cc.Color(0, 0, 0, 0))
-      this.drawManager.clear()
+    if (str === "clear") {
+      this.drawManager.setStrokeColor(new cc.Color(0, 0, 0, 0));
+      this.drawManager.clear();
     } else {
-      this.drawManager.setLineWidth(40)
-      this.drawManager.setStrokeColor(new cc.Color(0, 0, 0, 0))
+      this.drawManager.setLineWidth(10);
+      this.drawManager.setStrokeColor(new cc.Color(0, 0, 0, 0));
     }
   }
 
   onPen() {
-    this.drawManager.setLineWidth(10)
-    this.drawManager.setStrokeColor(new cc.Color(200, 0, 200, 255))
+    this.drawManager.setLineWidth(10);
+    this.drawManager.setStrokeColor(new cc.Color(200, 0, 200, 255));
   }
 
   // update (dt) {}
